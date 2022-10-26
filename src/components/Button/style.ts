9a3918d27bd8d53ef499f9ae2e4c12170b41db5c
Anete Neto <anetebuconeto@gmail.com>
2022-10-26 23:32:05 +0100

    Initial commit
    Generated by create-expo-app 1.1.2.

---
+++ src/components/Button/style.ts
@@ -0,0 +1,17 @@
+import { StyleSheet} from "react-native";
+
+
+export const styles=StyleSheet.create({
+  buttonText:{
+    color:"#fff",
+    fontSize:16
+  },
+  button:{
+    width:56,
+    height:56,
+    borderRadius:5,    
+    backgroundColor:'pink',
+    alignItems:'center',
+    justifyContent:'center',
+  },
+});
